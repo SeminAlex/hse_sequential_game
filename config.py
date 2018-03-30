b6a33fe1a1057361a1714d6fb298cac65322a93a
SeminAlex <semin.aleks@list.ru>
2018-03-30 19:28:59 +0300

    global recreation is started

--- config.py
+++ config.py
@@ -1,4 +1,53 @@
+# Units database, information taken from
+# http://ru.mightandmagic.wikia.com/wiki/Heroes_of_Might_and_Magic_V
+# key - unit name, value - list(attack, defense, damage, life, initiative, speed, range)
 Units = {
-    "archer": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
-    "slender_men": [1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, ],
+    "Peasant": [1, 1, 1, 3, 8, 4, 1],
+    "Archer": [4, 3, 2, 7, 9, 4, 10],
+    "Marksman": [4, 4, 2, 10, 8, 4, 12],
+    "Footman": [4, 8, 2, 16, 8, 4, 1],
+    "Squire": [5, 9, 2, 26, 8, 4, 1],
+    "Griffin": [7, 5, 5, 30, 15, 7, 1],
+    "Imperial griffin": [9, 8, 5, 35, 15, 7, 1],
+    "Priest": [12, 12, 9, 54, 10, 5, 7],
+    "Inquisitor": [16, 16, 9, 80, 10, 5, 7],
+    "Angel": [27, 27, 45, 180, 11, 6, 1],
+    "Archangel": [31, 31, 50, 220, 11, 8, 1],
+    "Gremlin": [2, 2, 1, 5, 7, 3, 5],
+    "Master gremlin": [2, 2, 1, 6, 11, 5, 7],
+    "Stone gargoyle": [3, 4, 1, 15, 9, 6, 1],
+    "Obsidian gargoyle": [3, 5, 1, 20, 10, 7, 1],
+    "Iron golem": [5, 5, 3, 18, 7, 4, 1],
+    "Steel golem": [6, 6, 5, 24, 7, 4, 1],
+    "Mage": [10, 10, 7, 18, 10, 4, 3],
+    "Archmage": [10, 10, 7, 30, 10, 4, 4],
+    "Djinn": [13, 12, 12, 33, 12, 7, 1],
+    "Djinn sultan": [14, 14, 14, 45, 12, 8, 1],
+    "Rakshasa rani": [25, 20, 15, 120, 9, 5, 1],
+    "Rakshasa raja": [25, 20, 23, 140, 8, 6, 1],
+    "Colossus": [27, 27, 40, 175, 10, 6, 1],
+    "Titan": [30, 30, 40, 190, 10, 6, 5],
+    "Imp": [2, 1, 1, 4, 13, 5, 1],
+    "Familiar": [3, 2, 2, 6, 13, 5, 1],
+    "Horned demon": [1, 3, 1, 13, 7, 5, 1],
+    "Horned overseer": [3, 4, 1, 13, 8, 5, 1],
+    "Hell hound": [4, 3, 3, 15, 13, 7, 1],
+    "Cerberus": [4, 2, 4, 15, 13, 8, 1],
+    "Succubus": [6, 6, 6, 20, 10, 4, 6],
+    "Succubus mistress": [6, 6, 6, 30, 10, 4, 6],
+    "Hell charger": [13, 13, 8, 50, 16, 7, 1],
+    "Nightmare": [18, 18, 8, 66, 16, 8, 1],
+    "Pit fiend": [21, 21, 13, 110, 8, 4, 1],
+    "Pit lord": [22, 21, 13, 120, 8, 4, 1],
+    "Devil": [27, 25, 36, 166, 11, 7, 1],
+    "Arch devil": [32, 29, 36, 199, 11, 7, 1],
 }
+
+attributes = list(["attack", "defense", "damage", "life", "initiative", "speed", "range"])
+units_names = dict(map(lambda x:(x[1],x[0]), enumerate(Units.keys())))
+
+print(units_names)
+etalon = len(attributes)
+for i in Units:
+    if len(Units[i]) != etalon:
+        print("Unit - {}, length = {}".format(i, len(Units[i])))
--- game.py
+++ game.py
@@ -1,12 +1,7 @@
 from enum import Enum
 from copy import deepcopy as cp
 from random import randrange, sample
-from config import Units
-
-game_matrix = list()  # matrix asossiated with game field
-
-N = 10
-M = 12
+import config as cfg
 
 
 def _create_zero_line(length):
@@ -17,28 +12,18 @@ class Status(Enum):
     win = 1
     lose = -1
     draw = 0
-    isnot_ended = -2
+    not_ended = -2
 
 
 class Game:
     __slots__ = ["field_", "player_", "units_", "height_", "weight_", "FEATURE_NUM", "status", ]
     """
     field_ - matrix: FEATURE_NUM x (height*weight), where each row has length= height*weight
-             field_[0] - binary array, 1 means there is unit in this location, and 0 - otherwise
-             field_[1-10] - arrays associated with unit features for FIRST player : 
-                                 1) attack, 2) defence, 3) damage min, 4) damage max, 5) health, 6) initiative, 
-                                 7) speed, 8) shoots(??), 9) spellpoints(??), 10) b-pop(???)
-             field_[11-20] - arrays associated with unit features for SECOND player :
-                                 11) attack, 12) defence, 13) damage min, 14) damage max, 15) health, 16) initiative,
-                                 17) speed, 18) 1shoots(??), 19) spellpoints(??), 20) b-pop(???)
-             field_[21] - binary array with range, where current unit can move(1 means unit can move in location, 
-                            0 - otherwise) 
-             field_[22] - binary array with location which current unit can attack ONLY if he made movement in this 
-                           round: 1 means that location can be attacked by unit, 0 - otherwise
-             field_[23] - binary array with locations that can be attacked if unit didn't made movements in this round:
-                            1 means location can be attacked, 0 - otherwise
-             field_[24] - 
-             
+             field_[0-FEATURE_NUM] - binary arrays, 1 in raw i means, that unit, which name is unit_names[i] is located 
+                                     in this cell and it is all units of FIRST player
+             field_[FEATURE_NUM-FEATURE_NUM*2] - binary arrays, same as previous but for SECOND player
+             field_[FEATURE_NUM*2+1] -  
+                          
     
     player_       - binary variable that indicate which turn is now
     height_       - int variable, shows height of field
@@ -47,11 +32,11 @@ class Game:
     status        - Status variable, can be any value from 'Status' class
     """
 
-    def __init__(self, height=15, weight=11, units=None, field=None):
+    def __init__(self, height, weight, units=None, field=None):
         self.height_ = height
         self.weight_ = weight
-        self.FEATURE_NUM = 24
-        self.status = Status.isnot_ended
+        self.FEATURE_NUM = len(cfg.units_names)
+        self.status = Status.not_ended
         if field:
             self.field_ = cp(field)
         else:
@@ -65,7 +50,7 @@ class Game:
             if len(units) < 1 or len(units[0]) != 4:
                 raise Exception("ERROR: List of units must contain at least one tuple like (X, Y, UNIT_NAME, PLAYER)")
             for x, y, unit_name, player in units:
-                attributes = Units.get(unit_name, None)
+                attributes = cfg.Units.get(unit_name, None)
                 if not attributes:
                     raise Exception("ERROR: Unit with name {} doesn't exist. Possible unit names : {}"
                                     .format(unit_name, Units.keys()))
@@ -94,8 +79,6 @@ class Game:
         return False
 
 
-
-
 def genereate_units_array(number, player_percent, weight, height):
     def indext2coordinate(index):
         w = index // weight
@@ -109,10 +92,11 @@ def genereate_units_array(number, player_percent, weight, height):
     return units
 
 
-game = Game()
+game = Game(15, 11)
 
-#tmp = genereate_units_array(8, 0.5, 15, 11)
-tmp =[(2, 3, 'slender_men', 1), (2, 13, 'slender_men', 1), (9, 13, 'slender_men', 1), (7, 9, 'archer', 1), (9, 9, 'archer', 0), (1, 3, 'slender_men', 0), (1, 4, 'archer', 0), (2, 8, 'archer', 0)]
+# tmp = genereate_units_array(8, 0.5, 15, 11)
+tmp = [(2, 3, 'slender_men', 1), (2, 13, 'slender_men', 1), (9, 13, 'slender_men', 1), (7, 9, 'archer', 1),
+       (9, 9, 'archer', 0), (1, 3, 'slender_men', 0), (1, 4, 'archer', 0), (2, 8, 'archer', 0)]
 print(tmp)
 game = Game(weight=15, height=11, units=tmp)
 print(game)
